1a4c6417e28f4ae310e9da472ecc164c8619292f
Asanka Perera <47624621+pereraasa@users.noreply.github.com>
2021-12-08 10:57:56 +0530

    Update install.sh

--- 00-template/kubernetes/05-efk-logging/install.sh
+++ 00-template/kubernetes/05-efk-logging/install.sh
@@ -3,5 +3,5 @@
 kubectl create ns logging
 
 helm repo add akomljen-charts https://raw.githubusercontent.com/komljen/helm-charts/master/charts/
-helm install --name es-operator  --namespace logging  akomljen-charts/elasticsearch-operator
-helm install --name efk --namespace logging akomljen-charts/efk --values=./efk-values.yaml
+helm install es-operator  --namespace logging  akomljen-charts/elasticsearch-operator
+helm install efk --namespace logging akomljen-charts/efk --values=./efk-values.yaml
